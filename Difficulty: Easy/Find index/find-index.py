e349a14de862b9b398186b2dd50ec5f3ad963ded
Vangapally Bhanu_Dev <114730455+Bhanu-Vangapally@users.noreply.github.com>
2025-10-23 23:26:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find index/find-index.py
@@ -0,0 +1,10 @@
+#User function Template for python3
+
+arr = tuple(map(int, input().split()))
+x = int(input())
+print(arr.index(x))
+
+########### Write your code below ###############
+# Print the index of x in arr
+
+########### Write your code above ###############

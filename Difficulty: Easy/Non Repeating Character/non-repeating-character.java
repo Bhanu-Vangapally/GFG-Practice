59ad0544ef971efd7376d5ef52d86dfbfa3c2ae8
Vangapally Bhanu_Dev <114730455+Bhanu-Vangapally@users.noreply.github.com>
2025-12-28 16:31:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Non Repeating Character/non-repeating-character.java
+++ Difficulty: Easy/Non Repeating Character/non-repeating-character.java
@@ -1,13 +1,10 @@
 class Solution {
     public char nonRepeatingChar(String s) {
         // code here
-        Map<Character,Integer>m=new LinkedHashMap<>();
-        for(char i:s.toCharArray()){
-            m.put(i,m.getOrDefault(i,0)+1);
-        }
-        // System.out.print(m);
-        for(Map.Entry<Character,Integer> e:m.entrySet()){
-            if(e.getValue()==1) return e.getKey();
+        Map<Character,Integer> m=new LinkedHashMap<>();
+        for(char i:s.toCharArray()) m.put(i,m.getOrDefault(i,0)+1);
+        for(char c:m.keySet()){
+            if(m.get(c)==1) return c;
         }
         return '$';
     }

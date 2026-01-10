805d842eedc613f21ebb80b8b7c3824d3f9d6e76
Vangapally Bhanu_Dev <114730455+Bhanu-Vangapally@users.noreply.github.com>
2026-01-11 00:32:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Even Odd - Java/even-odd-java.java
@@ -0,0 +1,11 @@
+// Function to check if number is even or odd
+class Geeks {
+    //
+    static void evenOdd(int a, int b) {
+        // Your code here
+        int evn=a%2==0?a:b;
+        int odd=a%2!=0?a:b;
+        System.out.println(evn);
+        System.out.println(odd);
+    }
+}
